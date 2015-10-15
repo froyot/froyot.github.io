6aaf17e9545d95a0c506bb25abc1eaefdd2c025b
Allon <xianlong.wang@dev-engine.com>
2015-10-15 15:22:19 +0800

    edit post Yii2 themes record

--- _posts/2015-10-15-Yii2-themes-record.php
+++ _posts/2015-10-15-Yii2-themes-record.php
@@ -4,13 +4,17 @@ title: Yii2 主题记录
 category: 技术
 comments: true
 ---
+
+
 ## Yii2 主题添加记录
 Yii2本身支持多模块，多主题开发。因此对开发中遇到的多主题问题，以及自定
 义不同模块主题需要的注意事项进行记录
 
 *   主题添加
 默认全局主题在配置文件中添加comment配置
+
 ```php
+
  'view' => [
             'theme' => [
                 'basePath' => '@app/themes/{themesName}',
@@ -20,18 +24,24 @@ Yii2本身支持多模块，多主题开发。因此对开发中遇到的多主�
                 ],
             ],
   ]
+
 ```
+
 *   多模块定义主题
 
 如果有多个不同的模块，想要在不同的模块中设置不同的模板，在模块的入口文
 件中添加
 
-```
+
+```php
+
         \Yii::$app->view->theme = new \yii\base\Theme([
             'pathMap' => ['@app/views' => '@app/admin/views'],
             'baseUrl' => '@web',
         ]);
+
 ```
+
 *  文件机制
 YII2中主题layout文件机制，如果定义了重新定义了@app/views，则在该目录下搜
 索layout文件夹中的布局文件，如果没有，则在默认文件中寻找。其他页面的view
@@ -47,6 +57,7 @@ YII2中，web可以访问的目录是限制在@web目录下。所以以上情况
 立一个ThemeAsset文件
 
 ```php
+
 namespace app\themes\tfviolet;
 
 use yii\web\AssetBundle;
@@ -64,6 +75,7 @@ class ThemeAsset extends AssetBundle
         'js/materialize.min.js'
     ];
 }
+
 ```
 
 注意，一定要定义sourcePath，只有这样才能将sourcePath的所有文件发布到asset目
